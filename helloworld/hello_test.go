6156bfb1d6f4932363c8cf7083600fe9be2d9104
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-10-13 18:01:24 +0700

    first commit

---
+++ helloworld/hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// Tell the test suite that this method
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
