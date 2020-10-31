1657aaf6fa68b7017351ad6ff82c7214305ea394
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-11-01 06:27:38 +0700

    Learn to write test on http server

---
+++ http-server/main.go
@@ -0,0 +1,14 @@
+package http_server
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := &PlayerServer{NewInMemoryPlayerStore()}
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
